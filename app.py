335fcc1f7e54cdbb5f982b125b4ae99afab4a908
Juan-Diego-Ortiz-Baquero <bakerojuandiego@gmail.com>
2025-11-12 10:37:46 -0500

    Update app to use environment variable for port and disable debug mode

--- app.py
+++ app.py
@@ -878,4 +878,6 @@ if __name__ == '__main__':
     print("🌐 Servidor ejecutándose en: http://localhost:5000")
     print("=" * 70)
     print("")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
